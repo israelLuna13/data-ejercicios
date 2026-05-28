511758fac870a40e3f80d09349168630aead6c8e
israelLuna13 <c24.e.israel.luna@gmail.com>
2026-05-27 20:01:20 -0600

    ejercicio terminado

--- eje-9/querys.sql
+++ eje-9/querys.sql
@@ -39,3 +39,42 @@ CREATE TABLE fact_udemy (
     discount_price_amount NUMERIC(10,2),
     price_detail_amount NUMERIC(10,2) NOT NULL
 );
+
+-- * Cuáles son los cursos con mejor rating
+SELECT fu.title, fu.avg_rating_recent 
+FROM fact_udemy fu
+ORDER BY fu.avg_rating_recent DESC
+--LIMIT 5
+;
+
+-- * Qué cursos tienen más suscriptores
+SELECT fu.title, SUM(fu.num_subscribers) sum_subscribers    
+FROM fact_udemy fu
+GROUP BY fu.title
+ORDER BY sum_subscribers DESC
+--LIMIT 5
+;
+-- * Cuántos cursos son gratuitos vs pagados
+SELECT  fu.is_paid, COUNT(fu.is_paid) as course
+FROM fact_udemy fu
+GROUP BY fu.is_paid
+-- * Los cursos pagados tienen mejor rating que los gratuitos
+SELECT  fu.is_paid, AVG(fu.avg_rating) as avg_rating
+FROM fact_udemy fu
+GROUP BY fu.is_paid;
+-- * Cuáles son los cursos más caros
+SELECT fu.title, fu.price_detail_amount
+FROM fact_udemy fu
+ORDER BY fu.price_detail_amount DESC
+--LIMIT 5
+;
+-- * Qué cursos tienen más reviews
+SELECT fu.title, fu.num_reviews
+FROM fact_udemy fu
+ORDER BY fu.num_reviews DESC
+--LIMIT 5
+-- * Qué cursos tienen más clases publicadas
+SELECT fu.title, fu.num_published_practice_tests
+FROM fact_udemy fu
+ORDER BY fu.num_published_practice_tests DESC
+--LIMIT 5
